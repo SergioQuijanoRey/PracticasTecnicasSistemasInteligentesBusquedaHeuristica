a6ae3642731db3f93e7d25d845b12751a2fd19c9
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-03-19 13:14:29 +0100

    feat: intento escribir un agente basico pero da problemas de copilacion

---
+++ src/quijano_rey_sergio/Agent.java
@@ -0,0 +1,114 @@
+// El paquete que se nos pide que usemos
+package quijano_rey_sergio;
+
+/**
+ * Referencias usadas para el desarrollo de la practica:
+ *
+ * [1] http://www.gvgai.net/cont.php:
+ *  - Pequeño tutorial sobre como crear un agente
+ * */
+
+// TODO -- Sergio -- limpiar todo esto porque no se si sobran
+
+import core.game.Game;
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+import ontology.Types;
+import tools.Direction;
+import tools.ElapsedCpuTimer;
+import tools.Utils;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+
+// Para generar numeros aleatorios
+import java.util.Random;
+
+/**
+ * Codigo del agente inteligente que vamos a desarrollar para resolver el juego
+ * de Boulder Dash
+ *
+ * Tenemos que extender core.player.AbstractPlayer.java como se indica en [1]
+ * @author Sergio Quijano Rey
+ * */
+public class Agent extends core.player.AbstractPlayer{
+    /**
+     * Constructor del agente.
+     * Tiene que recibir esos parametros de entrada porque asi se indica en [1]
+     * @param so estado del mundo, dado como una observacion
+     * @param elapsedTimer timer que nos va a indicar el tiempo de computo que tenemos
+     *
+     * Tiene un segundo de computo
+     * */
+    public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer){}
+
+    /**
+     * Metodo que se llama al inicio de cada nivel de un juego
+     * Para realizar computos al inicio de un juego
+     * @param sso observacion del juego al inicio de este
+     * @param elapsedTimer timer que nos da 1s de computo
+     * */
+    // TODO -- Sergio -- Este codigo esta comentado, borrarlo
+    //@Override
+    //public void init(SerializableStateObservation sso, ElapsedCpuTimer elapsedTimer){
+    //}
+
+
+    /**
+     * Elige una accion conociendo el estado del mundo y el tiempo que ya esta consumido
+     * @param stateObs estado del mundo
+     * @param elapsedTimer para conocer cuanto tiempo hemos consumido. Permite
+     * hacer consultas sobre el tiempo consumido o el tiempo que tenemos restante
+     * @return la accion que debe realizar nuestro agente
+     *
+     * De momento estamos eligiendo una eleccion al azar entre las dos que tenemos
+     * */
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+        System.out.println("Estamos intentando pensar una accion");
+
+        //Direction move = Utils.processMovementActionKeys(Game.ki.getMask(), Types.DEFAULT_SINGLE_PLAYER_KEYIDX); //use primary set of keys, idx = 0
+        //boolean useOn = Utils.processUseKey(Game.ki.getMask(), Types.DEFAULT_SINGLE_PLAYER_KEYIDX); //use primary set of keys, idx = 0
+
+        //Types.ACTIONS action = Types.ACTIONS.fromVector(move);
+        //if(action == Types.ACTIONS.ACTION_NIL && useOn){
+        //    action = Types.ACTIONS.ACTION_USE;
+
+
+        //}
+
+        // Generamos una accion aleatoria y devolvemos una accion segun eso
+        Random rand = new Random();
+        int random_value = rand.nextInt(2);
+        Types.ACTIONS action;
+
+        if(random_value == 0){
+            action = Types.ACTIONS.ACTION_DOWN;
+        }else{
+            action = Types.ACTIONS.ACTION_RIGHT;
+        }
+
+        System.out.println("Estamos devolviendo una accion");
+
+
+        return action;
+    }
+
+    /**
+     * Metodo que se llama cuando una partida se ha acabado.
+     * Para realizar computos a posteriori (teardown o procesar datos)
+     *
+     * @param sso el estado del juego al terminar
+     * @param elapsedTimer timer con restriccion hasta llegar a CompetitionParameters.TOTAL_LEARNING_TIME
+     * @return el siguiente nivel del juego actual para jugar
+     * */
+    //@Override
+    //public int result(SerializableStateObservation sso, ElapsedCpuTimer elapsedTimer){
+    //    Random r = new Random();
+    //    Integer level = r.nextInt(3);
+
+    //    return level;
+    //}
+
+
+}
