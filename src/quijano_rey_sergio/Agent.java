da207d07b2b20f32126befbb615d6df79a0d4d35
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-03-19 13:54:47 +0100

    feat: estructura para saber en que nivel estamos (not impl) y actuar segun el nivel en el que nos encontremos

--- src/quijano_rey_sergio/Agent.java
+++ src/quijano_rey_sergio/Agent.java
@@ -30,6 +30,13 @@ import java.util.Random;
  * */
 public class Agent extends core.player.AbstractPlayer{
     /**
+     * Nivel actual en el que nos encontramos.
+     * Lo conocemos viendo si hay gemas en el mapa y si hay enemigos en el mapa
+     * Segun el nivel en el que nos encontremos actuamos de un modo u otro
+     * */
+    int current_level = -1;
+
+    /**
      * Constructor del agente.
      * Tiene que recibir esos parametros de entrada porque asi se indica en [1]
      * @param so estado del mundo, dado como una observacion
@@ -38,45 +45,59 @@ public class Agent extends core.player.AbstractPlayer{
      * Tiene un segundo de computo
      * */
     public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer){
+        // TODO -- Sergio -- borrar estos mensajes por pantalla
         System.out.println("Estamos construyendo la clase");
+
+        // Mirando si hay gemas y enemigos, establecemos el nivel en el que
+        // nos encontramos
+        this.current_level = this.get_level(so, elapsedTimer);
     }
 
     /**
-     * Metodo que se llama al inicio de cada nivel de un juego
-     * Para realizar computos al inicio de un juego
-     * @param sso observacion del juego al inicio de este
-     * @param elapsedTimer timer que nos da 1s de computo
-     * */
-    // TODO -- Sergio -- Este codigo esta comentado, borrarlo
-    //@Override
-    @Override
-    public void init(SerializableStateObservation sso, ElapsedCpuTimer elapsedTimer){
-        System.out.println("Estoy haciendo preprocesados");
-    }
-
-
-    /**
-     * Elige una accion conociendo el estado del mundo y el tiempo que ya esta consumido
+     * Elige una accion conociendo el estado del mundo, el nivel y el tiempo que ya esta consumido
      * @param stateObs estado del mundo
      * @param elapsedTimer para conocer cuanto tiempo hemos consumido. Permite
      * hacer consultas sobre el tiempo consumido o el tiempo que tenemos restante
      * @return la accion que debe realizar nuestro agente
      *
-     * De momento estamos eligiendo una eleccion al azar entre las dos que tenemos
+     * Dependiendo del nivel en el que estemos, llamamos a uno de los metodos
+     * que eligen accion segun el nivel
      * */
     @Override
     public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+        switch(this.current_level){
+            case 1:
+                return this.leven1_act(stateObs, elapsedTimer);
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                // TODO -- Sergio -- no mostrar mensajes por pantalla
+                System.err.println("[Err] Todavia no hemos implementado este nivel");
+                System.err.println("Devolvemos accion nula");
+                return Types.ACTIONS.ACTION_NIL;
+            default:
+                // TODO -- Sergio -- no mostrar mensajes por pantalla
+                System.err.println("[Err] El valor actual de nivel no es valido");
+                System.err.println("Devolvemos accion nula");
+                return Types.ACTIONS.ACTION_NIL;
+        }
 
-        //Direction move = Utils.processMovementActionKeys(Game.ki.getMask(), Types.DEFAULT_SINGLE_PLAYER_KEYIDX); //use primary set of keys, idx = 0
-        //boolean useOn = Utils.processUseKey(Game.ki.getMask(), Types.DEFAULT_SINGLE_PLAYER_KEYIDX); //use primary set of keys, idx = 0
 
-        //Types.ACTIONS action = Types.ACTIONS.fromVector(move);
-        //if(action == Types.ACTIONS.ACTION_NIL && useOn){
-        //    action = Types.ACTIONS.ACTION_USE;
+    }
 
-
-        //}
-
+    /**
+     * Elige una accion en la situacion en la que estemos en el nivel 1.
+     *
+     * En este caso, hacemos A* para calcular el camino hacia la salida y devolvemos
+     * en cada paso la accion correspondiente
+     *
+     * Los calculos solo se hacen una vez. Cuando el plan ya esta construido (no
+     * es vacio) simplemente devolvemos el siguiente elemento
+     *
+     *
+     * */
+    public Types.ACTIONS leven1_act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
         // Generamos una accion aleatoria y devolvemos una accion segun eso
         Random rand = new Random();
         int random_value = rand.nextInt(5);
@@ -95,20 +116,21 @@ public class Agent extends core.player.AbstractPlayer{
         return action;
     }
 
-    /**
-     * Metodo que se llama cuando una partida se ha acabado.
-     * Para realizar computos a posteriori (teardown o procesar datos)
-     *
-     * @param sso el estado del juego al terminar
-     * @param elapsedTimer timer con restriccion hasta llegar a CompetitionParameters.TOTAL_LEARNING_TIME
-     * @return el siguiente nivel del juego actual para jugar
-     * */
-    @Override
-    public int result(SerializableStateObservation sso, ElapsedCpuTimer elapsedTimer){
-        System.out.println("Estamos viendo que hacer cuando ha acabado la partida");
-        Random r = new Random();
-        Integer level = r.nextInt(3);
 
-        return level;
+
+
+    /**
+     * Con los datos del entorno, establece en que nivel nos encontramos.
+     * Esto se hace viendo si hay no hay gemas en el mapa y viendo si hay o no
+     * hay enemigos en el mapa
+     *
+     * @param stateObs estado del mundo, del que tomamos los datos de enemigos y
+     * gemas
+     * @param elapsedTimer para conocer cuanto tiempo hemos consumido. Permite
+     * hacer consultas sobre el tiempo consumido o el tiempo que tenemos restante
+     * */
+    int get_level(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+        return 1;
     }
+
 }
